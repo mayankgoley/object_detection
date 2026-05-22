45dec0eb4a36033c9293ed5e3b8d8f393a5d4223
Mayank Goley <42241477+mayankgoley@users.noreply.github.com>
2026-05-22 01:45:49 -0700

    added mobile cam feature

--- webcam.py
+++ webcam.py
@@ -1,10 +1,13 @@
+import sys
 from ultralytics import YOLO
 import cv2
+
+cam = int(sys.argv[1]) if len(sys.argv) > 1 else 0
 
 model = YOLO("yolo11n.pt")
 
 results = model.track(
-    source=0,
+    source=cam,
     stream=True,
     conf=0.4,
     tracker="bytetrack.yaml",
